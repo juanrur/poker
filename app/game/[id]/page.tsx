316d38a97e06d3e579b51d1fdfed23cf193aa175
Juan Ruiz <juanruizcampa@gmail.com>
2026-01-20 21:37:05 +0100

    Agregar lógica para evaluar manos y determinar el ganador al finalizar la ronda en el componente Home

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -27,8 +27,91 @@ export default function Home() {
   
   const params = useParams();
 
+  function evaluateHand(playerCards: Card[], communityCards: Card[]): number {
+    let value = 0
+    const allCards = [...playerCards, ...communityCards]
+    const cardValues = allCards.map(card => card.number).sort((a, b) => b - a)
+
+    let timeCards : Record<number, number> = {}
+    cardValues.forEach((value) => {
+      timeCards[value] = (timeCards[value] ?? 0 ) + 1
+    })
+
+    // high card
+    value = cardValues[0]
+
+    // pair
+    const pairs = Object.entries(timeCards).filter(([key, count]) => count === 2).sort((a, b) => Number(b[0]) - Number(a[0]))
+    if(pairs.length > 0) {
+      value = 13 + Number(pairs[0][0])
+    }
+
+    // double pair
+    if(pairs.length > 1) {
+      value = 13 + (7 * Number(pairs[0][0])) + Number(pairs[1][0])
+    }
+
+    // three of a kind
+    const threeOfAKind = Object.entries(timeCards).filter(([key, count]) => count === 3).sort((a, b) => Number(b[0]) - Number(a[0]))
+    if(threeOfAKind.length > 0) {
+      value = 400 + Number(threeOfAKind[0][0])
+    }
+
+    // straight
+    const uniqueCardValues = Object.keys(timeCards).map(Number).sort((a, b) => b - a)
+    const isStraight = (values: number[]) => values.length >= 5 && values[0] - values[4] === 4
+    if(isStraight(uniqueCardValues)) {
+      value = 500 + uniqueCardValues[0]
+    }
+
+    // full house
+    if(threeOfAKind.length > 0 && pairs.length > 0) {
+      value = 600 + (Number(threeOfAKind[0][0])) + Number(pairs[0][0])
+    }
+
+    // four of a kind
+    const fourOfAKind = Object.entries(timeCards).filter(([key, count]) => count === 4).sort((a, b) => Number(b[0]) - Number(a[0]))
+    if(fourOfAKind.length > 0) {
+      value = 800 + Number(fourOfAKind[0][0])
+    }
+
+    // straight flush
+    const suitsCount: Record<suit, number[]> = { heart: [], spade: [], club: [], diamond: [] }
+    allCards.forEach(card => {
+      suitsCount[card.suit].push(card.number)
+    })
+    for (const suitKey in suitsCount) {
+      const numbers = suitsCount[suitKey as suit].sort((a, b) => b - a)
+      if(isStraight(numbers)) {
+        value = 900 + numbers[0]
+        break
+      }
+    }
+
+    // royal flush
+    for (const suitKey in suitsCount) {
+      const numbers = suitsCount[suitKey as suit].sort((a, b) => b - a)
+      if(isStraight(numbers) && numbers[0] === 13) {
+        value = 1000
+        break
+      }
+    }
+
+    return value
+  }
+
+  async function whoWin() {
+    const playerHands = players.map(player => ({ ...player, handValue: evaluateHand(player.cards, game.cards) }));
+    const sortedHands = playerHands.sort((a, b) => b.handValue - a.handValue);
+    const winner = sortedHands[0];
+    winner.money += players.reduce((acc, player) => acc + player.bet, 0)
+    await supabase.from('players').update({ money: winner.money }).eq('id', winner.id).select()
+    for (const player of players) {
+      await supabase.from('players').update({ bet: 0, is_folded: false, cards: [] }).eq('id', player.id).select()
+    }
+  }
+
   async function roundOver () {
-    const bote = players.forEach(player => player.bet)
     await supabase.from('games').update({turn_player: null}).eq('id', params.id).select()
     const updatedPlayers = players.map(player => ({...player, bet: 0, is_folded: false}))
     for (const player of updatedPlayers) {
@@ -36,6 +119,7 @@ export default function Home() {
     }
     setPlayers(updatedPlayers)
     await supabase.from('games').update({actual_bet: 0, street: 0, cards: [], has_incremented: false, deck: [], dealer: null}).eq('id', params.id).select()
+    whoWin()
 
   }
 
